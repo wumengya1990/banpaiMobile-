d6a6db58d30921ea7ecde0e09fee947b9a7856ae
wumengya1990 <wumengya@aliyun.com>
2018-11-07 17:44:45 +0800

    2018-11-08更新

--- src/page/components/classMasterIndex.vue
+++ src/page/components/classMasterIndex.vue
@@ -1,16 +1,79 @@
 <template>
     <div class="classMasterIndex bgColor fullScreen">
-        <div></div>
-        <div class="indexBox">
-            <h3><span>今日考勤统计</span></h3>
+        <div class="tubiao">
             
+        </div>
+        <div class="indexDataBox">
+            <h3><span>今日考勤统计</span></h3>
+            <div id="tongji1"></div>
+        </div>
+        <div class="indexDataBox">
+            <h3><span>信息发布周统计</span></h3>
+
         </div>
     </div>
 </template>
 
 <script>
+
 export default {
-name:"classMasterIndex"
+name:"classMasterIndex",
+data(){
+    return{
+       
+    }
+},
+mounted(){
+    this.jinriData();
+},
+methods:{
+    jinriData:function(){
+        
+        var echarts = require('echarts');
+        var myChart = echarts.init(document.getElementById('tongji1'));
+
+       myChart.setOption({
+                title : {
+                    text: '某站点用户访问来源',
+                    subtext: '纯属虚构',
+                    x:'center',
+                    show:false
+                },
+                tooltip : {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                    data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+                },
+                series : [
+                    {
+                        name: '访问来源',
+                        type: 'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data:[
+                            {value:335, name:'直接访问'},
+                            {value:310, name:'邮件营销'},
+                            {value:234, name:'联盟广告'},
+                            {value:135, name:'视频广告'},
+                            {value:1548, name:'搜索引擎'}
+                        ],
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            });
+
+    }
+}
 }
 </script>
 
