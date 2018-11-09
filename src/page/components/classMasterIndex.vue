930b0b95fdc30092f3399cae0cc1e272ad4f180d
wumengya1990 <wumengya@aliyun.com>
2018-11-09 17:47:44 +0800

    2018-11-09更新

--- src/page/components/classMasterIndex.vue
+++ src/page/components/classMasterIndex.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="indexDataBox">
             <h3><span>信息发布周统计</span></h3>
-
+            <div id="tongji2"></div>
         </div>
     </div>
 </template>
@@ -25,41 +25,36 @@ data(){
 },
 mounted(){
     this.jinriData();
+    this.zhouXinxi();
 },
 methods:{
     jinriData:function(){
-        
         var echarts = require('echarts');
         var myChart = echarts.init(document.getElementById('tongji1'));
-
        myChart.setOption({
-                title : {
-                    text: '某站点用户访问来源',
-                    subtext: '纯属虚构',
-                    x:'center',
-                    show:false
-                },
+                
                 tooltip : {
                     trigger: 'item',
                     formatter: "{a} <br/>{b} : {c} ({d}%)"
                 },
+                color:['#009afc','#22977c','#5b50ce','#ee4671','#f2b847'],
                 legend: {
-                    orient: 'vertical',
-                    left: 'left',
-                    data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+                    orient: 'horizontal',
+                    bottom: '10px',
+                    data: ['正常','迟到','请假','早退','旷课']
                 },
                 series : [
                     {
                         name: '访问来源',
                         type: 'pie',
                         radius : '55%',
-                        center: ['50%', '60%'],
+                        center: ['50%', '40%'],
                         data:[
-                            {value:335, name:'直接访问'},
-                            {value:310, name:'邮件营销'},
-                            {value:234, name:'联盟广告'},
-                            {value:135, name:'视频广告'},
-                            {value:1548, name:'搜索引擎'}
+                            {value:50, name:'正常'},
+                            {value:8, name:'迟到'},
+                            {value:1, name:'请假'},
+                            {value:12, name:'早退'},
+                            {value:6, name:'旷课'}
                         ],
                         itemStyle: {
                             emphasis: {
@@ -71,6 +66,53 @@ methods:{
                     }
                 ]
             });
+
+    },
+    zhouXinxi:function(){
+        var echarts = require('echarts');
+        var myChart = echarts.init(document.getElementById('tongji2'));
+
+       myChart.setOption({
+            
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            color:['#0096ff','#009842'],
+            legend: {
+                orient: 'horizontal',
+                bottom: '10px',
+                data: ['上周', '本周']
+            },
+            grid: {
+                right: '5%',
+                left:'5%',
+                top:'10%',
+                containLabel: true
+            },
+            yAxis: {
+                type: 'value',
+                boundaryGap: [0, 0.01]
+            },
+            xAxis: {
+                type: 'category',
+                data: ['周一','周二','周三','周四','周五']
+            },
+            series: [
+                {
+                    name: '上周',
+                    type: 'bar',
+                    data: [50, 60, 73, 52, 57]
+                },
+                {
+                    name: '本周',
+                    type: 'bar',
+                    data: [42, 54, 68, 46, 51]
+                }
+            ]
+        });
 
     }
 }
